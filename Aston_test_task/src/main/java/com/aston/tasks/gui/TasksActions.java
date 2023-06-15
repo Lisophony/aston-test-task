1a4d14519ac01b31d83a5c87552e913f1c4e7658
Liza Phan <liza.phan.truong@gmail.com>
2023-06-15 11:57:05 +0300

    Create class with tasks for gui version

---
+++ Aston_test_task/src/main/java/com/aston/tasks/gui/TasksActions.java
@@ -0,0 +1,35 @@
+package com.aston.tasks.gui;
+
+import java.util.Arrays;
+
+public class TasksActions {
+    public static String sayHello(String number) {
+        try {
+            return Integer.parseInt(number) > 7 ? "Привет!" : "";
+        }
+        catch (NumberFormatException e) {
+            return "Проверьте введённые данные";
+        }
+    }
+
+    public static String helloViacheslav(String input) {
+        return input.equals("Вячеслав") ? "Привет, Вячеслав" : "Нет такого имени";
+    }
+
+    public static String getArrayDivisibleBy3(String input) {
+        try {
+            int[] array = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+            StringBuilder sb = new StringBuilder();
+            Arrays.stream(array).forEach(x-> {
+                if (x % 3 == 0) {
+                    sb.append(x);
+                    sb.append(" ");
+                }
+            });
+            return sb.toString();
+        }
+        catch (NumberFormatException e) {
+            return "Проверьте введённые данные";
+        }
+    }
+}
