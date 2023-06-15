a269802a59c0c88d518c7f46bd2b01b9ce57b142
Liza Phan <liza.phan.truong@gmail.com>
2023-06-15 11:26:36 +0300

    Move Scanner reading to printDivisibleBy3 method

--- Aston_test_task/src/main/java/com/aston/tasks/Task3.java
+++ Aston_test_task/src/main/java/com/aston/tasks/Task3.java
@@ -3,13 +3,13 @@ package com.aston.tasks;
 import java.util.Arrays;
 
 public class Task3 extends BaseTask {
-    private static int[] getArray() {
-        String[] input = scanner.nextLine().split(" ");
+    public static int[] getArray(String[] input) {
+
         return Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
     }
 
     public static void printDivisibleBy3() {
-        Arrays.stream(getArray()).forEach(x-> {
+        Arrays.stream(getArray(scanner.nextLine().split(" "))).forEach(x-> {
             if (x % 3 == 0) {
                 System.out.println(x);
             }
