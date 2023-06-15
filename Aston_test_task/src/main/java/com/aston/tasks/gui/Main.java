051606404e441b91fd18144007850e60317ff190
Liza Phan <liza.phan.truong@gmail.com>
2023-06-15 11:27:21 +0300

    Add gui solution

---
+++ Aston_test_task/src/main/java/com/aston/tasks/gui/Main.java
@@ -0,0 +1,7 @@
+package com.aston.tasks.gui;
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+    }
+}
--- Aston_test_task/src/main/java/com/aston/tasks/gui/MainWindow.java
+++ Aston_test_task/src/main/java/com/aston/tasks/gui/MainWindow.java
@@ -1,9 +1,24 @@
-package com.aston.tasks;
+package com.aston.tasks.gui;
+
+import com.aston.tasks.Task3;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame implements ActionListener {
+    private final JLabel label1;
+    private final JLabel label2;
+    private final JLabel label3;
+    private final JTextField task1;
+    private final JTextField task2;
+    private final JTextField task3;
+    private final JTextField outputText1;
+    private final JTextField outputText2;
+    private final JTextField outputText3;
+    private final JButton confirm;
 
     public MainWindow() {
         super("Aston test tasks");
@@ -19,18 +34,19 @@ public class MainWindow extends JFrame {
         JPanel outputsPanel = new JPanel();
         outputsPanel.setLayout(new BoxLayout(outputsPanel, BoxLayout.Y_AXIS));
 
-        JLabel label1 = new JLabel("Say Hello");
-        JLabel label2 = new JLabel("Hi Viacheslav");
-        JLabel label3 = new JLabel("Print array");
-        JTextField task1 = new JTextField(10);
-        JTextField task2 = new JTextField(10);
-        JTextField task3 = new JTextField(10);
+        label1 = new JLabel("Say Hello");
+        label2 = new JLabel("Hi Viacheslav");
+        label3 = new JLabel("Print array");
+        task1 = new JTextField(10);
+        task2 = new JTextField(10);
+        task3 = new JTextField(10);
 
-        JButton confirm = new JButton("OK");
+        confirm = new JButton("OK");
+        confirm.addActionListener(this);
 
-        JTextField outputText1 = new JTextField(10);
-        JTextField outputText2 = new JTextField(10);
-        JTextField outputText3 = new JTextField(10);
+        outputText1 = new JTextField(10);
+        outputText2 = new JTextField(10);
+        outputText3 = new JTextField(10);
 
         Font textfieldFont = task1.getFont().deriveFont(Font.PLAIN, 14f);
 
@@ -41,6 +57,9 @@ public class MainWindow extends JFrame {
         outputText1.setFont(textfieldFont);
         outputText2.setFont(textfieldFont);
         outputText3.setFont(textfieldFont);
+        outputText1.setEditable(false);
+        outputText2.setEditable(false);
+        outputText3.setEditable(false);
 
         inputsPanel.add(label1);
         inputsPanel.add(task1);
@@ -71,5 +90,23 @@ public class MainWindow extends JFrame {
         mainPanel.add(Box.createRigidArea(new Dimension(40, 0)));
         getContentPane().add(mainPanel);
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        int number = Integer.parseInt(task1.getText());
+        if(number > 7) outputText1.setText("Привет!");
+
+        outputText2.setText(task2.getText().equals("Вячеслав") ? "Привет, Вячеслав" : "Нет такого имени");
+
+        int[] array = Task3.getArray(task3.getText().split(" "));
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(array).forEach(x-> {
+            if (x % 3 == 0) {
+                sb.append(x);
+                sb.append(" ");
+            }
+        });
+        outputText3.setText(sb.toString());
     }
 }
